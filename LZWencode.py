f7e00e819b087a9092c9962f64dacfce2704e3b3
Ye-Gon Ryoo <yr26@students.waikato.ac.nz>
2020-03-12 00:54:05 +1300

    Changed output format

--- LZWencode.py
+++ LZWencode.py
@@ -34,6 +34,6 @@ else:
 	mama_node.add_base_level_nodes(sorted(list(set(data))))
 	while len(data) > 0:
 		data = mama_node.find_and_add_data(data)
-	print(",".join([str(c.content) + "=" + str(c.index) for c in mama_node.children]) 
+	print(",".join([str(c.content) for c in mama_node.children]) 
 		+ " " 
 		+ ",".join([str(i) for i in TrieNode.index_coll]))

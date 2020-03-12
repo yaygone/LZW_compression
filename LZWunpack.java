75fb088698a213c7e441d6ebb97b0e8dc4696f94
Ye-Gon Ryoo <yr26@students.waikato.ac.nz>
2020-03-12 14:40:46 +1300

    More work on pack and unpack. Pack logic tidied up

--- LZWunpack.java
+++ LZWunpack.java
@@ -1,10 +1,41 @@
-class LZWunpack
+import java.util.ArrayList;
+
+LZWunpack
 {
 	String inputString = "";
 	public static void main(String[] args)
 	{
 		args[0].toCharArray();
 		
+	}
+
+	public byte[] process(byte[] input, int frameSize)
+	{
+		try
+		{
+			ArrayList<Byte> outputList = new ArrayList<Byte>();
+
+			byte tempData = 0;
+			int tempSize = 0;
+			int startIndex = 0;
+			byte currOutput = 0;
+
+			for (byte b : input)
+			{
+				tempData = b;
+				if (startIndex > frameSize)
+				{
+					
+				}
+
+				
+
+
+				startIndex = (startIndex + frameSize) % 8;
+			}
+		}
+		catch (Exception e) {}
+		return new byte[1];
 	}
 
 }
